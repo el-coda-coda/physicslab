5da6a537d665e401401e5fc7d0c0b09b83fdb214
el-coda-coda <codarin.andrea@gmail.com>
2025-09-25 01:13:52 +0200

    added StatisticVariables, StatisticalTests e inserted the possibility of using functions in inputs

--- physicslab-coda/statistics.py
+++ physicslab-coda/statistics.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Callable
 import numpy as np
 
 _ERR_XY_LENGTH = "x and y have different length"
@@ -6,6 +6,8 @@ _ERR_XERRX_LENGTH = "x and errx have different length"
 _ERR_YERRY_LENGTH = "y and erry have different length"
 _ERR_DIFF_LENGHT = "Input arrays have different length"
 _ERR_FEW_POINTS = "Number of points is not sufficient for this calculation"
+_ERR_EMPTY_ARRAY = "Input array is empty"
+_ERR_DEG_FREEDOM = "The number of degrees of freedom are wrong"
 
 class Interpolation:
     """
@@ -13,7 +15,6 @@ class Interpolation:
     <p>This class collects the interpolation methods used in data analysis in the physics laboratory.</p>
     <h3>Methods:</h3>
     <ul>
-        <li><code>linear_function(x, a, b)</code> Linear function</li>
         <li><code>erry_var_errx_null(x, y, erry, verbose=False)</code> Linear interpolation with variable error on Y and negligible error on X</li>
         <li><code>linear_interpolation(x, y, errx, erry, threshold=30, tries=10, verbose=False)</code> Linear interpolation with variable error on Y and variable error on X</li>
     </ul>
@@ -23,9 +24,6 @@ class Interpolation:
     def __init__(self) -> None:
         # This constructor is intentionally left empty as the class does not require initialization of instance variables.
         pass
-
-    def linear_function(self, x: np.ndarray, a: float, b: float) -> np.ndarray:
-        return a+b*x
     
     @staticmethod
     def erry_var_errx_null(x: np.ndarray, y: np.ndarray, erry: np.ndarray, verbose: bool=False) -> Tuple[float, float, float, float]:
@@ -33,6 +31,9 @@ class Interpolation:
         <p><code>erry_var_errx_null(x, y, erry, verbose=True)</code></p>
         <p>This function allows you to calculate the parameters and provide the intermediate details of the linear interpolation 
         of a sample of data. In this case, the error on Y will be variable and the error on X is negligible.</p>
+        <p><strong>Output:</strong></p>
+        <p>a: intercept, s_a: intercept error, b: slope, s_b: slope error</p>
+        <p><strong>Author:</strong> Andrea Codarin</p>
         """
         if(len(x)!=len(y)):
             raise ValueError(_ERR_XY_LENGTH)    
@@ -69,6 +70,9 @@ class Interpolation:
         <p>This function allows you to calculate the parameters and provide the intermediate details of the linear interpolation 
         of a sample of data. In this case, the error on Y will be variable and the error on X is variable, both are not neglegible.
         It choose the better approximation.</p>
+        <p><strong>Output:</strong></p>
+        <p>a: intercept, s_a: intercept error, b: slope, s_b: slope error</p>
+        <p><strong>Author:</strong> Andrea Codarin</p>
         """
         if(len(x)!=len(y)):
             raise ValueError(_ERR_XY_LENGTH)    
@@ -106,6 +110,11 @@ class StatisticalVariables:
     <h3>Methods:</h3>
     <ul>
         <li><code>r(x, y)</code> Calculate the sample correlation coefficient of a data set</li>
+        <li><code>mean(x)</code> Calculate the mean of a data set</li>
+        <li><code>var(x, sample=True)</code> Calculate the sample variance of a data set
+        <li><code>sdev(x, sample=True)</code> Calculate the sample standard deviation of a data set
+        <li><code>post_var(f, parameters, x, y)</code> Calculate the posterior variance of a data set
+        <li><code>cov(x, y, sample=True)</code> Calculate the sample covariance of a data set
     </ul>
     <p><strong>Author:</strong> Andrea Codarin</p>    
     """
@@ -114,11 +123,12 @@ class StatisticalVariables:
         # This constructor is intentionally left empty as the class does not require initialization of instance variables.
         pass    
 
-    @staticmethod
-    def r(x: np.ndarray, y: np.ndarray) -> float:
+    def r(self, x: np.ndarray, y: np.ndarray) -> float:
         """
         <p><code>coeff_corr(x, y)</code></p>
         <p>This function allows you to calculate the sample correlation coefficient of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>r: sample correlation coefficient</p>
         <p><strong>Author: </strong>Andrea Codarin</p>
         """
         n = len(x)
@@ -130,7 +140,88 @@ class StatisticalVariables:
 
         r = (n*sum_xy-sum_x*sum_y)/np.sqrt((n*sum_x2-sum_x**2)*(n*sum_y2-sum_y**2))
         return r
+    
+    def mean(self, x: np.ndarray) -> float:
+        """
+        <p><code>mean(x)</code></p>
+        <p>This function allows you to calculate the mean of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>mean: data set mean</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
+        """
+        if(len(x) == 0):
+            raise ValueError(_ERR_EMPTY_ARRAY)
+        n = len(x)
+        mean = np.sum(x)/n
+        return float(mean)
+    
+    def var(self, x: np.ndarray, sample: bool=True) -> float:
+        """
+        <p><code>variance(x, sample=True)</code></p>
+        <p>This function allows you to calculate the sample variance of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>var: data set variance</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
+        """
+        n = len(x)
+        mean = self.mean(x)
+        correction = 0
+        if(sample):
+            correction = 1
 
+        variance = np.sum((x-mean)**2)/(n-correction)
+        return float(variance)
+    
+    def sdev(self, x: np.ndarray, sample: bool=True) -> float:
+        """
+        <p><code>stddev(x, sample=True)</code></p>
+        <p>This function allows you to calculate the sample standard deviation of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>sdev: data set standard deviation</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
+        """
+        variance = self.var(x, sample=sample)
+        stddev = np.sqrt(variance)
+        return float(stddev)
+    
+    def post_var(self, f: Callable[[np.ndarray], float], parameters: int, x: np.ndarray, y: np.ndarray) -> float:
+        """
+        <p><code>post_variance(f, parameters, x, y)</code></p>
+        <p>This function allows you to calculate the posterior variance of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>pv: data set posterior variance</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
+        """
+        if(len(x)!=len(y)):
+            raise ValueError(_ERR_XY_LENGTH)    
+        
+        n = len(x)
+        if(n<=parameters):
+            raise ValueError(_ERR_FEW_POINTS)
+        
+        s = np.sum((y-f(x))**2)
+        pv = s/(n-parameters)
+        return float(pv)
+
+    def cov(self, x: np.ndarray, y: np.ndarray, sample: bool=True) -> float:
+        """
+        <p><code>covar(x, y, sample=True)</code></p>
+        <p>This function allows you to calculate the sample covariance of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>cov: data set covariance</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
+        """
+        if(len(x)!=len(y)):
+            raise ValueError(_ERR_XY_LENGTH)    
+        
+        n = len(x)
+        mean_x = self.mean(x)
+        mean_y = self.mean(y)
+        correction = 0
+        if(sample):
+            correction = 1
+        covariance = np.sum((x-mean_x)*(y-mean_y))/(n-correction)
+        return float(covariance)
 
 class StatisticalTests:
     """
@@ -138,112 +229,150 @@ class StatisticalTests:
     <p>This class collects the statistical tests used in data analysis in the physics laboratory.</p>
     <h3>Methods:</h3>
     <ul>
-        <li><code>chi2(parameters, y_original, y_calculated, erry)</code> Calculate the chi squared of a data set</li>
-        <li><code>reduced_chi2(parameters, y_original, y_calculated, erry)</code> Calculate the reduced chi squared of a data set</li>
-        <li><code>post_error(parameters, y_original, y_calculated)</code> Calculate the posterior error of a data set</li>
-        <li><code>r_t_student(x, y)</code> Calculate the r t-student value of a cluster of data</li>
-        <li><code>t_student(mean, stddev, n, mu)</code> Calculate the t-student value of a cluster of data</li>
-        <li><code>f_test(var1, var2, n1, n2)</code> Calculate the f-test value of two clusters of data</li>
-        <li><code>double_var_t_student(mean1, mean2, var1, var2, n1, n2)</code> Calculate the t-student value of two clusters of data with different variance</li>
+        <li><code>chi_sqrd(f, parameters, x, y, erry)</code> Calculate the chi squared of a data set</li>
+        <li><code>reduced_chi_sqrd(f, parameters, x, y, erry)</code> Calculate the reduced chi squared of a data set</li>
+        <li><code>post_error(f, parameters, x, y)</code> Calculate the posterior error of a data set</li>
+        <li><code>r_t_student(x, y)</code> Calculate the r-t-student value of a cluster of data</li>
+        <li><code>t_student(x, mu)</code> Calculate the t-student value of a cluster of data</li>
+        <li><code>alternative_f_test(f_0, f_a, v_0, v_a, x, y)</code> Calculate the f-test value of two clusters of data</li>
+        <li><code>t_student_comp(x, y)</code> Calculate the t-student value of two clusters of data with different variance</li>
     </ul>
     <p><strong>Author:</strong> Andrea Codarin</p>    
     """
 
     def __init__(self) -> None:
-        # This constructor is intentionally left empty as the class does not require initialization of instance variables.
-        pass
+        self.stat_variables = StatisticalVariables()
+        
 
-    def chi2(self, parameters: int, y_original: np.ndarray, y_calculated: np.ndarray, erry: np.ndarray) -> Tuple[float, int]:
+    def chi_sqrd(self, f: Callable[[np.ndarray], float], parameters: int, x: np.ndarray, y: np.ndarray, erry: np.ndarray) -> Tuple[float, int]:
         """
-        <p><code>chi_squared(y, y_calculated, erry)</code></p>
-        <p>This function allows you to calculate the chi squared of a data set (only for linear function).</p>
+        <p><code>chi_sqrd(f(np.ndarray) -> float, parameters, x, y, erry)</code></p>
+        <p>This function allows you to calculate the chi squared of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>chi2: data set χ squared, df: degrees of freedom</p>
         <p><strong>Author: </strong>Andrea Codarin</p>
         """
-        if(len(y_original)!=len(y_calculated)):
+        if(len(x)!=len(y)):
             raise ValueError(_ERR_DIFF_LENGHT)    
-        elif(len(y_original)!=len(erry)):
+        if(len(y)!=len(erry)):
             raise ValueError(_ERR_YERRY_LENGTH)
-        n = len(y_original)
+        
+        n = len(x)
+        
         if(n<=parameters):
             raise ValueError()
-        chi2 = np.sum(((y_original-y_calculated)/erry)**2)
+        
+        chi2 = np.sum(((y-f(x))/erry)**2)
         df = n-parameters
         return float(chi2), df
     
-    def reduced_chi2(self, parameters: int, y_original: np.ndarray, y_calculated: np.ndarray, erry: np.ndarray) -> Tuple[float, int]:
+    def reduced_chi_sqrd(self, f: Callable[[np.ndarray], float], parameters: int, x: np.ndarray, y: np.ndarray, erry: np.ndarray) -> Tuple[float, int]:
         """
-        <p><code>reduced_chi_squared(y, y_calculated, erry)</code></p>
-        <p>This function allows you to calculate the reduced chi squared of a data set (only for linear function).</p>
+        <p><code>reduced_chi_sqrd(f(np.ndarray) -> float, parameters, x, y, erry)</code></p>
+        <p>This function allows you to calculate the reduced chi squared of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>rchi2: data set χ squared reduced, df: degrees of freedom</p>
         <p><strong>Author: </strong>Andrea Codarin</p>
         """
-        chi2, df = self.chi2(parameters, y_original, y_calculated, erry)
+        chi2, df = self.chi_sqrd(f, parameters, x, y, erry)
         rchi2 = chi2/df
         return rchi2, df    
     
-    def post_error(self, parameters: int, y_original: np.ndarray, y_calculated: np.ndarray) -> Tuple[float, int]:
+    def post_error(self, f: Callable[[np.ndarray], float], parameters: int, x: np.ndarray, y: np.ndarray) -> Tuple[float, int]:
         """
-        <p><code>post_error(x, y, a, b)</code></p>
-        <p>This function allows you to calculate the posterior error of a data set (only for linear function).</p>
+        <p><code>post_error(f(np.ndarray) -> float, parameters, x, y)</code></p>
+        <p>This function allows you to calculate the posterior error of a data set.</p>
+        <p><strong>Output:</strong></p>
+        <p>pe: data set posterior error, df: degrees of freedom</p>
         <p><strong>Author: </strong>Andrea Codarin</p>
         """
-        if(len(y_original)!=len(y_calculated)):
-            raise ValueError(_ERR_DIFF_LENGHT)   
-        s=sum((y_original-y_calculated)**2)
-        df=len(y_calculated)-parameters 
-        pe=np.sqrt(s/(len(y_calculated)-parameters))
+        if(len(x)!=len(y)):
+            raise ValueError(_ERR_DIFF_LENGHT)  
+         
+        s=sum((y-f(x))**2)
+        df=len(y)-parameters 
+        pe=np.sqrt(s/(len(y)-parameters))
         return pe, df
     
     def r_t_student(self, x: np.ndarray, y: np.ndarray) -> Tuple[float, float]:
         """
         <p><code>r_t_student(x, y)</code></p>
-        <p>This function allows you to calculate the r t-student value of a cluster of data.</p>
+        <p>This function allows you to calculate the r-t-student value of a cluster of data.</p>
+        <p><strong>Output:</strong></p>
+        <p>t: data set t-student, df: degrees of freedom</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
         """
         if(len(x)!=len(y)):
             raise ValueError(_ERR_XY_LENGTH)    
         
         df = len(x)-2
-        r = StatisticalVariables.r(x, y)
+        r = self.stat_variables.r(x, y)
         t = r*np.sqrt(df)/(np.sqrt(1-r**2))
         return t, df
     
-    def t_student(self, mean: float, stddev: float, n: int, mu: float) -> Tuple[float, int]:
+    def t_student(self, x: np.ndarray, mu: float) -> Tuple[float, int]:
         """
-        <p><code>t_student(mean, stddev, n, mu)</code></p>
+        <p><code>t_student(x, mu)</code></p>
         <p>This function allows you to calculate the t-student value of a cluster of data.</p>
+        <p><strong>Output:</strong></p>
+        <p>t: data set t-student, df: degrees of freedom</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
         """
+        n = len(x)
         if(n<2):
-            raise ValueError(_ERR_FEW_POINTS)    
-        
+            raise ValueError(_ERR_FEW_POINTS)
+            
+        mean = self.stat_variables.mean(x)
+        sdev = self.stat_variables.sdev(x)
         df = n-1
-        t = (mean-mu)/(stddev/np.sqrt(n))
+        t = (mean-mu)/(sdev/np.sqrt(n))
         return t, df
     
-    def f_test(self, var1: float, var2: float, n1: int, n2: int) -> Tuple[float, int, int]:
+    def alternative_f_test(self, f_0:Callable[[np.ndarray], float], f_a:Callable[[np.ndarray], float], v_0: int, v_a: int, x: np.ndarray, y: np.ndarray) -> Tuple[float, int, int]:
         """
-        <p><code>f_test(var1, var2, n1, n2)</code></p>
+        <p><code>alternative_f_test(f_0(np.ndarray) -> float, f_a(np.ndarray) -> float, v_0, v_a, x, y)</code></p>
         <p>This function allows you to calculate the f-test value of two clusters of data.</p>
+        <p><strong>Output:</strong></p>
+        <p>f_test: data set f_test, df1: degrees of freedom 1, df2: degrees of freedom 2</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
         """
-        if(n1<2 or n2<2):
-            raise ValueError(_ERR_FEW_POINTS)    
+        if(len(x)!=len(y)):
+            raise ValueError(_ERR_XY_LENGTH) 
+        if(len(x)<2):
+            raise ValueError(_ERR_FEW_POINTS)
+        if(v_0>=v_a):
+            raise ValueError(_ERR_DEG_FREEDOM)
         
-        if(var1>var2):
-            f = var1/var2
-            dfn = n1-1
-            dfd = n2-1
-        else:
-            f = var2/var1
-            dfn = n2-1
-            dfd = n1-1
-        return f, dfn, dfd  
+        n = len(y)
+        var_0 = self.stat_variables.post_var(f_0, v_0, x, y)
+        var_a = self.stat_variables.post_var(f_a, v_a, x, y)
+        f_test = ((n-v_0)*var_0-(n-v_a)*var_a)/(var_a*(v_a-v_0))
+        df1 = v_a-v_0
+        df2 = n-v_a
+        return f_test, df1, df2
     
-    def double_var_t_student(self, mean1: float, mean2: float, var1: float, var2: float, n1: int, n2: int) -> Tuple[float, int]:
+    def t_student_comp(self, x: np.ndarray, y: np.ndarray) -> Tuple[float, int]:
         """
-        <p><code>double_var_t_student(mean1, mean2, var1, var2, n1, n2)</code></p>
+        <p><code>t_student_comp(x, y)</code></p>
         <p>This function allows you to calculate the t-student value of two clusters of data with different variance.</p>
+        <p><strong>Output:</strong></p>
+        <p>t: data set t-student, df: degrees of freedom</p>
+        <p><strong>Author: </strong>Andrea Codarin</p>
         """
-        if(n1<2 or n2<2):
-            raise ValueError(_ERR_FEW_POINTS)    
+        if(len(x)==0 or len(y)==0):
+            raise ValueError(_ERR_EMPTY_ARRAY)
         
-        df = ((var1/n1)+(var2/n2))**2/((var1/n1)**2/(n1-1)+(var2/n2)**2/(n2-1))
-        t = (mean1-mean2)/np.sqrt(var1/n1+var2/n2)
-        return t, int(df)
+        n_x = len(x)
+        n_y = len(y)
+        df = n_x+n_y-2
+
+        if(df < 1):
+            raise ValueError(_ERR_DEG_FREEDOM)
+
+        mean_x = self.stat_variables.mean(x)
+        mean_y = self.stat_variables.mean(y)
+        var_x = self.stat_variables.var(x)
+        var_y = self.stat_variables.var(y)
+        s = ((n_x-1)*var_x+(n_y-1)*var_y)/df
+        t = np.abs((mean_x-mean_y))/(s*np.sqrt(1/n_x+1/n_y))
+        return t, df
